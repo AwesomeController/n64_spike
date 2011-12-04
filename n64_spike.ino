0480cf2b8b1be4522ce6985bb54f427a8bf62a24
Kyle Shipley <shipstar@gmail.com>
2011-12-03 20:43:24 -0500

    Blind hacking on timing issues.

--- n64_spike.ino
+++ n64_spike.ino
@@ -1,39 +1,69 @@
 #include "Command.h"
 
+#define N64_HIGH DDRD &= ~0x04
+#define N64_LOW DDRD |= 0x04
+#define N64_QUERY (PIND & 0x04)
+
 int DATA_PIN = 2;
+int buttons[33] = {
+  0, 0, 0, 1, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0,
+  0
+};
+
+volatile int readCounter = 0;
+int currentCommand[9];
 
 void setup() {
+  Serial.begin(14400);
+  
   attachInterrupt(0, respondToN64, FALLING);
   
   pinMode(DATA_PIN, INPUT);
-  digitalWrite(DATA_PIN, HIGH);
+  N64_HIGH;
 }
 
 void loop() {
-  
 }
 
 void respondToN64() {
-  noInterrupts();
-  
   Command c = readCommand();
   switch(c)
   {
     case READ_BUTTONS:
+      delayMicroseconds(100);
       sendButtons();
       break;
     default:
       break;
   }
-  
-  interrupts();
 }
 
 Command readCommand() {
-  int currentCommand[9];
+  return READ_BUTTONS;
   
-  for (int i = 0; i < 9; i++) {
-    currentCommand[i] << digitalRead(DATA_PIN);
+  int commandLength = sizeof(currentCommand) / sizeof(currentCommand[0]);
+  
+  for (int i = 0; i < commandLength; i++) {
+    delayTwoMicroseconds();
+    currentCommand[i] = N64_QUERY;
+    if (i != 8) {
+       if (!finishCycle()) {
+         return UNKNOWN;
+       }
+    }
+  }
+  
+  if (readCounter > 200) {
+    Serial.println("Current");    
+    for (int i = 0; i < commandLength; i++) {
+      Serial.println(currentCommand[i]);
+    }
+    readCounter = 0;
+  } else {
+    readCounter++;
   }
   
   int getButtonsCommand[9] = {0, 0, 0, 0, 0, 0, 0, 1, 1};
@@ -46,7 +76,66 @@ Command readCommand() {
 }
 
 void sendButtons() {
-  pinMode(DATA_PIN, OUTPUT);
-  // send the buttons
-  pinMode(DATA_PIN, INPUT);
+  volatile int length = sizeof(buttons) / sizeof(buttons[0]);
+  
+  if (readCounter > 200){
+    Serial.println(length);
+  } else {
+    readCounter++;
+  }  
+  
+  
+  for (int i = 0; i < length; i++)
+  {
+    delayMicroseconds(1);
+    if (buttons[i]) {
+      N64_HIGH;
+      delayMicroseconds(3);
+    } else {
+      N64_LOW;
+      delayMicroseconds(2);
+      N64_HIGH;
+      delayMicroseconds(1);
+    }
+  }
+  N64_HIGH;
+}
+
+void delayTwoMicroseconds() {
+  asm volatile (
+    "nop\nnop\nnop\nnop\nnop\n"
+    "nop\nnop\nnop\nnop\nnop\n"
+    "nop\nnop\nnop\nnop\nnop\n"
+    "nop\nnop\nnop\nnop\nnop\n"
+    "nop\nnop\nnop\nnop\nnop\n"
+    "nop\nnop\nnop\nnop\nnop\n"
+  );
+}
+
+boolean finishCycle() {
+  waitForHigh() && waitForLow();
+}
+
+boolean waitForHigh() {
+  int timeout = 63;
+  while (!N64_QUERY) {
+    if (!--timeout) {
+      Serial.println("high");
+      return false;
+    }
+  }
+  
+  return true;
+}
+
+boolean waitForLow() {
+  int timeout = 63;
+  while (N64_QUERY) {
+    if (!--timeout) {
+      Serial.println("low");
+      return false;
+    }
+  }
+  
+  return true;
 }
