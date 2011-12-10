f033c0c8a2298e465642198ffcdbaeb5a60236bd
Kail Keusch lt <hj497@cummins.com>
2011-12-10 14:29:07 -0500

    customization of N64_Arduino to mimick controller instead of N64

--- n64_spike.ino
+++ n64_spike.ino
@@ -46,21 +46,11 @@ void setup()
 {
   Serial.begin(115200);
 
-
-
   // Communication with gamecube controller on this pin
   // Don't remove these lines, we don't want to push +5V to the controller
   digitalWrite(N64_PIN, LOW);  
   pinMode(N64_PIN, INPUT);
-
-
-  // Initialize the gamecube controller by sending it a null byte.
-  // This is unnecessary for a standard controller, but is required for the
-  // Wavebird.
-  unsigned char initialize = 0x00;
-  noInterrupts();
-  N64_send(&initialize, 1);
-
+  
   // Stupid routine to wait for the gamecube controller to stop
   // sending its response. We don't care what it is, but we
   // can't start asking for status if it's still responding
@@ -71,15 +61,6 @@ void setup()
       if (!N64_QUERY)
           x = 0;
   }
-
-  // Query for the gamecube controller's status. We do this
-  // to get the 0 point for the control stick.
-  unsigned char command[] = {0x01};
-  N64_send(command, 1);
-  // read in data and dump it to N64_raw_dump
-  N64_get();
-  interrupts();
-  translate_raw_data();  
 }
 
 void translate_raw_data()
@@ -137,7 +118,7 @@ void N64_send(unsigned char *buffer, char length)
 outer_loop:
     {
         asm volatile (";Starting inner for loop");
-        bits=8;
+        bits=32;
 inner_loop:
         {
             // Starting a bit, set the line low
@@ -237,14 +218,15 @@ void N64_get()
     // Afterwards, call translate_raw_data() to interpret the raw data and pack
     // it into the N64_status struct.
     asm volatile (";Starting to listen");
-    unsigned char timeout;
-    char bitcount = 32;
+    unsigned char timeout1;
+    unsigned char timeout2;
+    char bitcount = 8;
     char *bitbin = N64_raw_dump;
 
     // Again, using gotos here to make the assembly more predictable and
     // optimization easier (please don't kill me)
 read_loop:
-    timeout1 = 0x3f;
+    timeout1 = 0xff;
     // wait for line to go low
     while (N64_QUERY) {
         if (!--timeout1)
@@ -267,9 +249,9 @@ read_loop:
 
     // wait for line to go high again
     // it may already be high, so this should just drop through
-    timeout = 0x3f;
+    timeout2 = 0x3f;
     while (!N64_QUERY) {
-        if (!--timeout)
+        if (!--timeout2)
             return;
     }
     goto read_loop;
@@ -336,15 +318,15 @@ void loop()
 
     // don't want interrupts getting in the way
     noInterrupts();
-    // send those 3 bytes
-    N64_send(command, 1);
     // read in data and dump it to N64_raw_dump
     N64_get();
+    // send those 3 bytes
+//    N64_send(command, 1);
     // end of time sensitive code
     interrupts();
 
     // translate the data in N64_raw_dump to something useful
-    translate_raw_data();
+//    translate_raw_data();
 
    for (i=0; i<16; i++) {
        Serial.print(N64_raw_dump[i], DEC);
